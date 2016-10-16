c13946dca5e81983508a90df52a556fa141ce8c7
Risto Saarelma <risto.saarelma@iki.fi>
2016-10-16 20:23:58 +0300

    Remove old emacs conf

--- .emacs.d/utils.el
+++
@@ -1,4 +0,0 @@
-(defun ensure-installed (package)
-  "Install a package using ELPA if it's not already installed"
-  (unless (package-installed-p package) (package-install package))
-  (require package))
