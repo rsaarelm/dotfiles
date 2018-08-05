234b7bceb648cbb4007f41926e568ac69f44f3dc
Risto Saarelma <risto.saarelma@iki.fi>
2018-08-05 12:22:37 +0300

    Add tantalum (laptop) configuration

---
+++ nixos/hosts/tantalum.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./common.nix
+      ./wifi.nix
+      ./home-network.nix
+      ./hardware-configuration.nix
+    ];
+
+  # BOOT
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # NETWORK
+
+  networking.hostName = "tantalum";
+
+  # HARDWARE
+
+  hardware = {
+    # Steam games want this.
+    opengl.driSupport32Bit = true;
+    pulseaudio.support32Bit = true;
+
+    bluetooth = {
+      enable = true;
+      extraConfig = ''
+        [General]
+        Enable=Source,Sink,Media,Socket
+      '';
+    };
+  };
+
+  # SERVICES
+
+  services = {
+    xserver = {
+      # Touchpad
+      libinput = {
+        enable = true;
+        disableWhileTyping = true;
+      };
+    };
+  };
+
+  # PACKAGES
+
+  environment.systemPackages = with pkgs; [
+    discord
+    steam
+    dropbox
+  ];
+}
