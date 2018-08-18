ccd5ee1c30f2a57bbd86a68e6cc790b3a20ea31c
Risto Saarelma <risto.saarelma@iki.fi>
2018-08-18 22:28:17 +0300

    DPI adjustment in Tungsten autorandr
    This actually works really nicely, neat. Programs like shells, nvim and
    chrome need to be shut down and restarted after the switch, but i3
    resizes its components and so forth.
    
    Also make the stow script do smart autorandr stow.

--- stow.sh
+++ stow.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 # List all subdirectories that should be stowed to $HOME here.
 
 stow core
-stow nix
 stow i3
 
 stow mplayer
 stow nvim
+
+AUTORANDR=autorandr-`hostname`
+if [ -e $AUTORANDR ]; then
+    stow $AUTORANDR
+fi
 
 stow factorio
