69757c800fb757c479fab9bc58a8ef7eddd9626b
Risto Saarelma <risto.saarelma@iki.fi>
2018-08-31 14:53:09 +0300

    Disable Tantalum touchpad when lid is closed
    It emits ghost inputs if it's left enabled.

--- nixos/hosts/tantalum.nix
+++ nixos/hosts/tantalum.nix
@@ -46,6 +46,21 @@
     };
 
     autorandr.enable = true;
+
+    acpid = {
+      enable = true;
+
+      # Disable touchpad when lid is closed, otherwise it randomly emits mouse noise.
+      lidEventCommands = ''
+        LID_STATE=$(/run/current-system/sw/bin/awk '{print $2}' /proc/acpi/button/lid/LID/state)
+        case $LID_STATE in
+          closed)
+            DISPLAY=:0.0 XAUTHORITY=/home/rsaarelm/.Xauthority /run/current-system/sw/bin/xinput --disable "Elan Touchpad";;
+          open)
+            DISPLAY=:0.0 XAUTHORITY=/home/rsaarelm/.Xauthority /run/current-system/sw/bin/xinput --enable "Elan Touchpad";;
+        esac
+      '';
+    };
   };
 
   services.xserver.dpi = 120;
