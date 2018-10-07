1df1dc3a4fa66a36460b577f7924d568be874ea2
Risto Saarelma <risto.saarelma@iki.fi>
2018-10-07 15:38:02 +0300

    LaTeX setup for NixOS

--- nixos/common.nix
+++ nixos/common.nix
@@ -66,6 +66,26 @@
       xxd
       youtubeDL
 
+      # LaTeX
+      (texlive.combine {
+        inherit (texlive)
+          collection-basic
+          metafont
+          xits
+          collection-bibtexextra
+          collection-binextra
+          collection-context
+          collection-formatsextra
+          collection-fontutils
+          collection-langenglish
+          collection-latex
+          collection-latexextra
+          collection-latexrecommended
+          collection-pictures
+          collection-pstricks
+          collection-xetex;
+      })
+
       # Dev
       binutils
       clang
