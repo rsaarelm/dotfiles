c053fdf6ea743f0e906bc6e96c9a6d1bfe30e562
Risto Saarelma <risto.saarelma@iki.fi>
2019-12-02 20:45:46 +0200

    Rename stow dirs

--- stow.sh
+++ stow.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
 
-# List all subdirectories that should be stowed to $HOME here.
-
-stow core
-stow config-`hostname`
-stow i3
-
-stow nvim
-
-AUTORANDR=autorandr-`hostname`
-if [ -e $AUTORANDR ]; then
-    stow $AUTORANDR
-fi
-
-stow games
+stow stow-*
+stow $(hostname)-stow
