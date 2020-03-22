0586e54203dda72fce74eb504521e603abf34bc1
Risto Saarelma <risto.saarelma@iki.fi>
2020-03-22 16:06:47 +0200

    Wacom tablet support

--- nixos/hosts/tungsten.nix
+++ nixos/hosts/tungsten.nix
@@ -32,25 +32,30 @@
     };
   };
 
-  services.xserver.videoDrivers = ["nvidiaBeta"];
+  services.xserver = {
+    videoDrivers = ["nvidiaBeta"];
 
-  # Dual monitor setup
-  services.xserver.xrandrHeads = [
-    {
-      output = "DP-1";
-      primary = true;
-      monitorConfig = ''
-        Option "PreferredMode" "1920x1080"
-      '';
-    }
-    {
-      output = "DVI-I-1";
-      monitorConfig = ''
-        Option "PreferredMode" "1920x1200"
-        Option "Rotate" "left"
-      '';
-    }
-  ];
+    # Dual monitor setup
+    xrandrHeads = [
+      {
+        output = "DP-1";
+        primary = true;
+        monitorConfig = ''
+          Option "PreferredMode" "1920x1080"
+        '';
+      }
+      {
+        output = "DVI-I-1";
+        monitorConfig = ''
+          Option "PreferredMode" "1920x1200"
+          Option "Rotate" "left"
+        '';
+      }
+    ];
+
+    # Wacom tablet setup
+    wacom.enable = true;
+  };
 
   system.stateVersion = "18.03";
 }
