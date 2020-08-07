3714c08bb3cd8d4d19152ff376e256cfa81ddac8
rsaarelm <risto.saarelma@iki.fi>
2020-08-07 21:35:31 +0300

    Improve djvu MIME matching

--- nixpkgs/common.nix
+++ nixpkgs/common.nix
@@ -75,6 +75,7 @@
       "application/pdf" = "org.pwmt.zathura.desktop";
       "application/epub+zip" = "org.pwmt.zathura.desktop";
       "image/vnd.djvu" = "org.pwmt.zathura.desktop";
+      "image/vnd.djvu+multipage" = "org.pwmt.zathura.desktop";
 
       "image/gif" = "sxiv.desktop";
       "image/png" = "sxiv.desktop";
