e9e641a3740c4cd6ee39548ddcb33665098ccde3
Risto Saarelma <risto.saarelma@iki.fi>
2021-06-13 00:19:35 +0300

    New machine setup

---
+++ nixpkgs/neodymium.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+
+{
+  xdg.configFile."i3/status.toml".text = ''
+    theme = "plain"
+    icons = "awesome"
+
+    [[block]]
+    block = "net"
+    device = "wlp1s0"
+    format = "{ip}"
+  '';
+
+  imports = [
+    ./common.nix
+    ./packages.nix
+
+    ./style/dark-theme.nix
+  ];
+
+  home.packages = with pkgs; [
+  ];
+
+  xsession.profileExtra = ''
+  '';
+}
