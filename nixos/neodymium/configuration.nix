723f3cf87ff228b8d53817f14a1a4057c3314dcf
Risto Saarelma <risto.saarelma@iki.fi>
2021-06-13 17:33:04 +0300

    Tweak RaspPI config, GPU support

--- nixos/neodymium/configuration.nix
+++ nixos/neodymium/configuration.nix
@@ -4,6 +4,7 @@
   imports =
     [
       ./hardware-configuration.nix
+      "${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/936e4649098d6a5e0762058cb7687be1b2d90550.tar.gz" }/raspberry-pi/4"
     ];
 
   # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
@@ -12,7 +13,12 @@
   boot.loader.generic-extlinux-compatible.enable = true;
 
   networking.hostName = "neodymium"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  networking.wireless = {
+    enable = true;
+    userControlled.enable = true;
+    interfaces = [ "wlan0" ];
+  };
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
@@ -20,6 +26,8 @@
   networking.useDHCP = false;
   networking.interfaces.eth0.useDHCP = true;
   networking.interfaces.wlan0.useDHCP = true;
+
+  hardware.raspberry-pi."4".fkms-3d.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
