fb79b59297fea5194235eb84fdb47896aa1d4708
Risto Saarelma <risto.saarelma@iki.fi>
2021-06-20 14:59:20 +0300

    Decrease disk churn

--- nixos/neodymium/hardware-configuration.nix
+++ nixos/neodymium/hardware-configuration.nix
@@ -16,6 +16,7 @@
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
       fsType = "ext4";
+      options = [ "noatime" ];
     };
 
   fileSystems."/home" =
