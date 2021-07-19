75ff4ab9e0fdb31a9b421a28c0c25d31169bf912
Risto Saarelma <risto.saarelma@iki.fi>
2021-07-19 17:47:06 +0300

    Foliate in mimeApps

--- nixpkgs/gui-core.nix
+++ nixpkgs/gui-core.nix
@@ -56,4 +56,21 @@
   };
 
   services.xcape.enable = true;
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/pdf" = "org.pwmt.zathura.desktop";
+      "application/postscript" = "org.pwmt.zathura.desktop";
+      "image/vnd.djvu" = "org.pwmt.zathura.desktop";
+      "image/vnd.djvu+multipage" = "org.pwmt.zathura.desktop";
+
+      "application/epub+zip" = "com.github.johnfactotum.Foliate.desktop";
+
+      "image/gif" = "sxiv.desktop";
+      "image/png" = "sxiv.desktop";
+      "image/jpeg" = "sxiv.desktop";
+      "image/bmp" = "sxiv.desktop";
+    };
+  };
 }
--- nixpkgs/programs/zathura.nix
+++ nixpkgs/programs/zathura.nix
@@ -16,20 +16,4 @@
       set guioptions ""  # Don't show status bar by default
     '';
   };
-
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "application/pdf" = "org.pwmt.zathura.desktop";
-      "application/epub+zip" = "org.pwmt.zathura.desktop";
-      "application/postscript" = "org.pwmt.zathura.desktop";
-      "image/vnd.djvu" = "org.pwmt.zathura.desktop";
-      "image/vnd.djvu+multipage" = "org.pwmt.zathura.desktop";
-
-      "image/gif" = "sxiv.desktop";
-      "image/png" = "sxiv.desktop";
-      "image/jpeg" = "sxiv.desktop";
-      "image/bmp" = "sxiv.desktop";
-    };
-  };
 }
