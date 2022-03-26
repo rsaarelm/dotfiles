e2c400fdbf0d859aa0636eafba5a3405fd6f3ebb
Risto Saarelma <risto.saarelma@iki.fi>
2022-03-26 18:14:17 +0200

    Remove some nix stuff and update stateVersion just because

--- nixos/tungsten/configuration.nix
+++ nixos/tungsten/configuration.nix
@@ -35,15 +35,6 @@
 
   # SERVICES
 
-  virtualisation = {
-    docker.enable = true;
-
-    # This causes lots of upgrade churn, only turn it on when needed.
-    # virtualbox.host.enable = true;
-    # virtualbox.host.enableExtensionPack = true;
-    # virtualbox.guest.enable = true;
-  };
-
   services.xserver = {
     videoDrivers = [ "nvidia" ];
     # Dual monitor setup
@@ -66,42 +57,7 @@
 
     # Wacom tablet setup
     wacom.enable = true;
-
-    # Needed for trackball config.
-    libinput.enable = true;
   };
 
-  environment.etc = {
-    # Logitech Trackman Marble trackball configuration
-    #
-    # | button       | xorg id | libinput  |
-    # |--------------+---------+-----------|
-    # | big left     | 1       | BTN_LEFT  |
-    # | big right    | 3       | BTN_RIGHT |
-    # | little left  | 8       | BTN_SIDE  |
-    # | little right | 9       | BTN_EXTRA |
-    # Right small button (9) is scroll and middle-click (remapped to 2)
-
-    # FIXME: libinput version's button scrolling stopped working after update to NixOS 20.03
-    "X11/xorg.conf.d/50-marblemouse.conf".text = ''
-      Section "InputClass"
-        Identifier   "Marble Mouse"
-        MatchProduct "Logitech USB Trackball"
-
-        #Driver       "libinput"
-        #Option       "ScrollMethod"        "button"
-        #Option       "ScrollButton"        "9"
-        #Option       "MiddleEmulation"     "true"
-        #Option       "HorizontalScrolling" "false"
-        #Option       "ButtonMapping"       "1 2 3 4 5 6 7 8 2"
-
-        Driver       "evdev"
-        Option       "EmulateWheel"       "true"
-        Option       "EmulateWheelButton" "9"
-        Option       "ButtonMapping"      "1 2 3 4 5 6 7 8 2"
-      EndSection
-    '';
-  };
-
-  system.stateVersion = "18.03";
+  system.stateVersion = "21.11";
 }
