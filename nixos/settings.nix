4b7180f537c6ac581b381a21fe6d89bbb70d96e1
Risto Saarelma <risto.saarelma@iki.fi>
2022-06-03 16:28:03 +0300

    Don't disable actual esc with intercept

--- nixos/settings.nix
+++ nixos/settings.nix
@@ -58,7 +58,7 @@
     enable = true;
     plugins = [ pkgs.interception-tools-plugins.caps2esc ];
     udevmonConfig = ''
-      - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+      - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc -m 1 | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
         DEVICE:
           EVENTS:
             EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
