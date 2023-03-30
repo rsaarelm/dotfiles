f643ad5de495c6a5846feb9972859f03d0d6cabb
Risto Saarelma <risto.saarelma@iki.fi>
2023-03-30 11:52:54 +0300

    PS4 controller steam support

--- nixos/tungsten/configuration.nix
+++ nixos/tungsten/configuration.nix
@@ -46,6 +46,38 @@
     };
   };
 
+  # PS4 controller support for Steam
+  # https://steamcommunity.com/app/221410/discussions/0/1693795812304458372/?l=polish
+  # https://discourse.nixos.org/t/steam-steam-run-and-joysticks/1451/2
+  services.udev.extraRules = ''
+    # This rule is needed for basic functionality of the controller in Steam and keyboard/mouse emulation
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
+
+    # This rule is necessary for gamepad emulation
+    KERNEL=="uinput", MODE="0660", GROUP="rsaarelm", OPTIONS+="static_node=uinput"
+
+    # Valve HID devices over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0666"
+
+    # Valve HID devices over bluetooth hidraw
+    KERNEL=="hidraw*", KERNELS=="*28DE:*", MODE="0666"
+
+    # DualShock 4 over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0666"
+
+    # DualShock 4 wireless adapter over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ba0", MODE="0666"
+
+    # DualShock 4 Slim over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="09cc", MODE="0666"
+
+    # DualShock 4 over bluetooth hidraw
+    KERNEL=="hidraw*", KERNELS=="*054C:05C4*", MODE="0666"
+
+    # DualShock 4 Slim over bluetooth hidraw
+    KERNEL=="hidraw*", KERNELS=="*054C:09CC*", MODE="0666"
+  '';
+
   # hiDPI settinps
   hardware.video.hidpi.enable = true;
   services.xserver.dpi = 192;
