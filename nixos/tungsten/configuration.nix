b9825db459c0dd928d19b491c60ed08e73cd40fb
Risto Saarelma <risto.saarelma@iki.fi>
2023-04-21 13:41:53 +0300

    Reactivate trackball config

--- nixos/tungsten/configuration.nix
+++ nixos/tungsten/configuration.nix
@@ -87,5 +87,37 @@
     _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
   };
 
+  environment.etc = {
+    # Logitech Trackman Marble trackball configuration
+    #
+    # | button       | xorg id | libinput  |
+    # |--------------+---------+-----------|
+    # | big left     | 1       | BTN_LEFT  |
+    # | big right    | 3       | BTN_RIGHT |
+    # | little left  | 8       | BTN_SIDE  |
+    # | little right | 9       | BTN_EXTRA |
+    # Right small button (9) is scroll and middle-click (remapped to 2)
+
+    # FIXME: libinput version's button scrolling stopped working after update to NixOS 20.03
+    "X11/xorg.conf.d/50-marblemouse.conf".text = ''
+      Section "InputClass"
+        Identifier   "Marble Mouse"
+        MatchProduct "Logitech USB Trackball"
+
+        #Driver       "libinput"
+        #Option       "ScrollMethod"        "button"
+        #Option       "ScrollButton"        "9"
+        #Option       "MiddleEmulation"     "true"
+        #Option       "HorizontalScrolling" "false"
+        #Option       "ButtonMapping"       "1 2 3 4 5 6 7 8 2"
+
+        Driver       "evdev"
+        Option       "EmulateWheel"       "true"
+        Option       "EmulateWheelButton" "9"
+        Option       "ButtonMapping"      "1 2 3 4 5 6 7 8 2"
+      EndSection
+    '';
+  };
+
   system.stateVersion = "21.11";
 }
