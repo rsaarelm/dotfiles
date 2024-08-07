dfc29b88238aeb05ef4a5ea7fd6842a33d0af9a3
Risto Saarelma <risto.saarelma@iki.fi>
2024-08-07 11:14:20 +0300

    Reactivate redshift

--- homegit/hosts/common.nix
+++ homegit/hosts/common.nix
@@ -41,6 +41,24 @@
   # At daemon
   services.atd.enable = true;
 
+  location = {
+    provider = "manual";
+    latitude = 60.0;
+    longitude = 25.0;
+  };
+
+  services.redshift = {
+    enable = true;
+    brightness = {
+      day = "1";
+      night = "1";
+    };
+    temperature = {
+      day = 5500;
+      night = 3700;
+    };
+  };
+
   # Automatically garbage collect after a while.
   nix.gc = {
     automatic = true;
